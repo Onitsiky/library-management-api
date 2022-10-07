e6bb7b9fff0845d492895d47cc8a7daa76d0d458
AmourRamanantsiresy <hei.amour.2@gmail.com>
2022-10-07 13:46:33 +0300

    feat: add database connection

---
+++ src/repository/connection.js
@@ -0,0 +1,27 @@
+import pg from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config({path: '.env'})
+const { Client } = pg;
+
+/**
+ * class client
+ * 
+ * how to use 
+ * Just import the client here
+ * 
+ * Connect to the database with client.connect()
+ * 
+ * And make your query like this:
+ * client.query('the query', (err, res) => {
+ *      console.log(res.rows);
+ * })
+ */
+const client = new Client({
+    port: process.env.APP_PORT || '',
+    password: process.env.APP_PASSWORD || '',
+    user: process.env.APP_USER || '',
+    database: process.env.APP_DATABASE || ''
+});
+
+export default client;
